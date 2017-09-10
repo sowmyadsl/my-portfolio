315dcb8e6b6c1f7458651c95e8110964fa9e684b
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-09-10 12:58:14 -0700

    Update portolfio script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,37 +1,24 @@
-# importing some modules
 import boto3
-# from botocore.client import Config
-# import StringIO
-# try:
-#     from StringIO import StringIO
-# except ImportError:
-#     from io import StringIO
+
 import io
 from botocore.client import Config
 import zipfile
 import mimetypes
 
-#In case this command doesn't work
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-#creating a name for s3 resource
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    portfolio_bucket = s3.Bucket('portfolio.sowmyadinavahi.me')
+    build_bucket = s3.Bucket('portfoliobuild.sowmyadinavahi.me')
 
-#creating names for both buckets we created using s3
-portfolio_bucket = s3.Bucket('portfolio.sowmyadinavahi.me')
-build_bucket = s3.Bucket('portfoliobuild.sowmyadinavahi.me')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-#creating string IO file in memory for portfolio_zip and downloading it
-# portfolio_zip = StringIO.StringIO()
-# portfolio_zip = io.StringIO()
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-
-# with the zipfile to extract, upload and set the ACL
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print "Job Done!"
+     return 'Hello from Lambda'
