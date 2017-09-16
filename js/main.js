a38add410f98f13fa1d25769958b646a3050484d
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-09-16 01:13:00 -0700

    Add a component to show work example in a modal and add interactivity and functionality to close it

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import ExampleWork from './example-work';
 const myWork = [
   {
     'title': "Work Example",
+    'href': "https://www.example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "Example of a react project ",
       'src': "images/ideaboard-react.png",
@@ -13,6 +15,8 @@ const myWork = [
   },
   {
     'title': "Photoshoot rails application",
+    'href': "https://www.example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "Example screenshot of application created using rails",
       'src': "images/photoshoot.png",
@@ -21,6 +25,8 @@ const myWork = [
   },
   {
     'title': "work Example",
+    'href': "https://www.example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "Restaurant API created using rails",
       'src': "images/api.jpg",
